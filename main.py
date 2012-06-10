82f656dfa2fb982db4d26a90487b0101733e302a
Ed Burton <ed@edburton.net>
2012-06-10 18:54:05 +0100

    Creating navigation structure

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
 
+site_name = 'OpenOrgs.org'
+site_slogan = 'for getting together whenever forever'
+
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
@@ -24,6 +27,20 @@ def guestbook_key(guestbook_name=None):
   return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
 
 
+def base_dictionary(self):
+    if users.get_current_user():
+        url = users.create_logout_url(self.request.uri)
+        url_linktext = 'Logout'
+    else:
+        url = users.create_login_url(self.request.uri)
+        url_linktext = 'Login'
+    return {
+            'url':url,
+            'url_linktext':url_linktext,
+            'site_name': site_name,
+            'site_slogan': site_slogan
+            }
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         guestbook_name = self.request.get('guestbook_name')
@@ -31,23 +48,42 @@ class MainPage(webapp2.RequestHandler):
             guestbook_key(guestbook_name)).order('-date')
         greetings = greetings_query.fetch(10)
 
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            url_linktext = 'Logout'
-        else:
-            url = users.create_login_url(self.request.uri)
-            url_linktext = 'Login'
-
-        template_values = {
-            'site_name': 'OpenOrgs.org', 
-            'site_slogan': 'for getting together whenever forever',
-            'greetings': greetings,
-            'url': url,
-            'url_linktext': url_linktext,
-        }
+        template_values = { 'greetings': greetings }
+        
+        template_values = dict(template_values.items() + base_dictionary(self).items())
 
         temp = os.path.join(os.path.dirname(__file__), 'templates/index.html')
-        outstr = template.render(temp,template_values)
+        outstr = template.render(temp, template_values)
+        self.response.out.write(outstr)
+
+class ManageEventsPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = { }
+        
+        template_values = dict(template_values.items() + base_dictionary(self).items())
+
+        temp = os.path.join(os.path.dirname(__file__), 'templates/manageevents.html')
+        outstr = template.render(temp, template_values)
+        self.response.out.write(outstr)
+        
+class ContactsPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = { }
+        
+        template_values = dict(template_values.items() + base_dictionary(self).items())
+
+        temp = os.path.join(os.path.dirname(__file__), 'templates/contacts.html')
+        outstr = template.render(temp, template_values)
+        self.response.out.write(outstr)
+        
+class AddContactsPage(webapp2.RequestHandler):
+    def get(self):
+        template_values = { }
+        
+        template_values = dict(template_values.items() + base_dictionary(self).items())
+
+        temp = os.path.join(os.path.dirname(__file__), 'templates/addcontacts.html')
+        outstr = template.render(temp, template_values)
         self.response.out.write(outstr)
                                  
 class Guestbook(webapp2.RequestHandler):
@@ -68,5 +104,7 @@ class Guestbook(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
-                               ('/sign', Guestbook)],
+                               ('/events/manage', ManageEventsPage),
+                               ('/contacts', ContactsPage),
+                               ('/contacts/add', AddContactsPage)],
                               debug=True)
