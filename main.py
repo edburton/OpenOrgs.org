89787942addc96e5eb209dfb0062c96ef48a0f82
Ed Burton <ed@edburton.net>
2012-06-17 14:16:58 +0100

    Model for Events and Users starting to "work", event input/display formatting

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import urllib
 import webapp2
 import jinja2
 import os
+import logging
 
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
-
-site_name = 'OpenOrgs.org'
-site_slogan = 'for getting together whenever forever'
+from webapp2_extras import sessions
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
@@ -20,50 +19,76 @@ jinja_environment = jinja2.Environment(
 class User(db.Model):
     email = db.EmailProperty()
     contacts = db.ListProperty(db.Key)
-    events = db.ListProperty(db.Key)
     
 class Event(db.Model):
-    title = str()
-    description = db.Text()
+    organizer = db.ReferenceProperty(reference_class=User)
+    title = db.StringProperty()
+    description = db.StringProperty(multiline=True)
 
 class EventInstance(db.Model):
     date = datetime.date
     
-################CONTROLLER##########################    
+################CONTROLLER##########################   
 
-class Greeting(db.Model):
-  """Models an individual Guestbook entry with an author, content, and date."""
-  author = db.UserProperty()
-  content = db.StringProperty(multiline=True)
-  date = db.DateTimeProperty(auto_now_add=True)
+class BaseHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
 
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
 
-def guestbook_key(guestbook_name=None):
-  """Constructs a Datastore key for a Guestbook entity with guestbook_name."""
-  return db.Key.from_path('Guestbook', guestbook_name or 'default_guestbook')
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session() 
 
 
 def base_dictionary(self):
+    dictionary = { }
+    dictionary['site_name'] = self.app.config.get('site_name')
+    dictionary['site_slogan'] = self.app.config.get('site_slogan')
     if users.get_current_user():
-        url = users.create_logout_url(self.request.uri)
-        url_linktext = 'Logout'
+        dictionary['log_in_or_out_url'] = users.create_logout_url(self.request.uri)
+        dictionary['log_in_or_out_text'] = 'logout'
+        dictionary['user_email'] = users.get_current_user().email()
     else:
-        url = users.create_login_url(self.request.uri)
-        url_linktext = 'Login'
-    return {
-            'url_linktext':url_linktext,
-            'site_name': site_name,
-            'site_slogan': site_slogan
-            }
+         dictionary['log_in_or_out_url'] = users.create_login_url(self.request.uri)
+         dictionary['log_in_or_out_text'] = 'login'
+    return dictionary
+
+def return_current_user(self):
+    if (users.get_current_user()):
+        email = users.get_current_user().email();
+        if email:
+            que = db.Query(User)
+            que = que.filter('email =', email)
+            user_s = que.fetch(limit=1)
+            if len(user_s):
+                return user_s[0]
+            else:
+                user = User(email=email)
+                user.put()
+                return user
+        
+def return_current_users_events(self):
+    user = return_current_user(self)
+    if user:
+        que = db.Query(Event)
+        que = que.filter('organizer =', user)
+        events_s = que.fetch(limit=None)
+        if len(events_s):
+            return events_s
+        
+        
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        guestbook_name = self.request.get('guestbook_name')
-        greetings_query = Greeting.all().ancestor(
-            guestbook_key(guestbook_name)).order('-date')
-        greetings = greetings_query.fetch(10)
-
-        template_values = { 'greetings': greetings }
+        template_values = { }
         
         template_values = dict(template_values.items() + base_dictionary(self).items())
 
@@ -86,7 +111,9 @@ class ManageEventsPage(webapp2.RequestHandler):
         template_values = { }
         
         template_values = dict(template_values.items() + base_dictionary(self).items())
-
+        events = return_current_users_events(self)
+        if events:
+            template_values['events']=events;
         temp = os.path.join(os.path.dirname(__file__), 'templates/manageevents.html')
         outstr = template.render(temp, template_values)
         self.response.out.write(outstr)
@@ -100,6 +127,20 @@ class AddEventPage(webapp2.RequestHandler):
         temp = os.path.join(os.path.dirname(__file__), 'templates/addevent.html')
         outstr = template.render(temp, template_values)
         self.response.out.write(outstr)
+        
+    def post(self):
+        user = return_current_user(self)
+        title = self.request.get('title')
+        description = self.request.get('description')
+        logging.info("user.email %s", user.email)
+        logging.info("title %s", title)
+        logging.info("description %s", description)
+        event = Event(parent=user)
+        event.organizer = user;
+        event.title = title;
+        event.description = description;
+        event.put();
+        self.redirect("/events/manage")
 
         
 class ContactsPage(webapp2.RequestHandler):
@@ -121,29 +162,21 @@ class AddContactsPage(webapp2.RequestHandler):
         temp = os.path.join(os.path.dirname(__file__), 'templates/addcontacts.html')
         outstr = template.render(temp, template_values)
         self.response.out.write(outstr)
-                                 
-class Guestbook(webapp2.RequestHandler):
-  def post(self):
-    # We set the same parent key on the 'Greeting' to ensure each greeting is in
-    # the same entity group. Queries across the single entity group will be
-    # consistent. However, the write rate to a single entity group should
-    # be limited to ~1/second.
-    guestbook_name = self.request.get('guestbook_name')
-    greeting = Greeting(parent=guestbook_key(guestbook_name))
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+config = {'site_name' : 'OpenOrgs.org',
+          'site_slogan' : 'for getting together whenever forever' }
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'OpenOrgs',
+}
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/?' + urllib.urlencode({'guestbook_name': guestbook_name}))
-
-
-app = webapp2.WSGIApplication([('/', MainPage),
+routes = [('/', MainPage),
                                ('/login', LoginPage),
                                ('/events/manage', ManageEventsPage),
                                ('/events/add', AddEventPage),
                                ('/contacts', ContactsPage),
-                               ('/contacts/add', AddContactsPage)],
+                               ('/contacts/add', AddContactsPage)]
+
+app = webapp2.WSGIApplication(routes=routes,
+                              config=config,
                               debug=True)
 
